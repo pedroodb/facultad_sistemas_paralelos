82f3822a675a726712ab0159083b472a9651a801
pedroodb <dalbianco.pedro@gmail.com>
2019-04-06 22:44:15 -0300

    Funciona ej2

--- Practica_2/ej2.c
+++ Practica_2/ej2.c
@@ -3,6 +3,16 @@
 #include <pthread.h>
 #include <time.h>
 
+//Para calcular tiempo
+double dwalltime(){
+  double sec;
+  struct timeval tv;
+
+  gettimeofday(&tv,NULL);
+  sec = tv.tv_sec + tv.tv_usec/1000000.0;
+  return sec;
+}
+
 int N, NUM_THREADS, NUM;
 int* vector;
 int* result;
@@ -18,25 +28,38 @@ void* buscar(void* id){
 }
 
 int main(int argc, char* argv[]){
-  N = atoi(argv[1]);
-  NUM_THREADS = atoi(argv[2]);
-  NUM = atoi(argv[3]);
+
+  //Controla los argumentos al programa
+  if ((argc != 3) || ((N = atoi(argv[2])) <= 0) )
+  {
+    printf("Error en cantidad de parametros");
+    exit(1);
+  }
+
+  srand(time(NULL));
+
+  double timetick;
+  NUM_THREADS = atoi(argv[1]);
+  N = atoi(argv[2]);
+  NUM = rand() % 5;
   block_size = N/NUM_THREADS;
   
   int ids[NUM_THREADS];  
   result = (int*)malloc(sizeof(int)*NUM_THREADS);
   vector = (int*)malloc(sizeof(int)*N);
-  srand(time(NULL));
 
   for(int i = 0; i < N; i++){
     vector[i] = rand() % 5;
-    printf(" %d", vector[i]);
   }
-  printf("\n");
+
   pthread_attr_t attr;
   pthread_t threads[NUM_THREADS];
   pthread_attr_init(&attr);
   
+  timetick = dwalltime();
+
+  //Lanza los threads para que realicen el calculo
+
   for(int i = 0; i < NUM_THREADS; i++){
     ids[i] = i;
     pthread_create(&threads[i], &attr, buscar, &ids[i]);
@@ -44,8 +67,13 @@ int main(int argc, char* argv[]){
   for(int i = 0; i < NUM_THREADS; i++){
     pthread_join(threads[i], NULL);
   }
+
+  //Calcula el resultado final sumando lo que calcularon los threads
+
   int res;
   for(int i = 0; i < NUM_THREADS; i++) res+= result[i];
-  printf("\n Result: %d\n", res);
+
+  float time = dwalltime() - timetick;
+  printf("%f\n", time);
 
 }
